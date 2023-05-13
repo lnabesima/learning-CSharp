c07c2836a0f0e399f23e023455b4b6b42e700215
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-13 12:52:01 -0300

    feat: create project 'criando-variaveis'

---
+++ CriandoVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Programa 2 - Criando Variáveis");
+        
+        int idade;
+        idade = 27;
+        Console.WriteLine($"Minha idade é {idade}.");
+
+        idade = idade - 5;
+        Console.WriteLine($"A idade da Fulana é {idade}.");
+
+        idade = (idade + 3) * 5;
+        Console.WriteLine($"Mas a idade do Sicrano é {idade}.");
+        
+        Console.WriteLine("Pressione Enter para fechar...");
+        Console.ReadLine();
+    }
+}
