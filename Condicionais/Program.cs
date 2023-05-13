3f7dea5751aefa908fbba94cfc71c467b118f47c
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-13 15:21:47 -0300

    feat: create 'Condicionais' project

---
+++ Condicionais/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Programa 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            if (quantidadePessoas > 1)
+            {
+                Console.WriteLine("Está acompanhado. Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+            }
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
