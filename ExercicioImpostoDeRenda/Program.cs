0401c0a9d3d1a23abe583037bf04c0f3411eb350
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-15 07:34:25 -0300

    feat: add 'ExercicioImpostoDeRenda' file

---
+++ ExercicioImpostoDeRenda/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 3300.0;
+        double aliquota = 0;
+        int restituicao = 0;
+
+        if (1900 < salario && salario <= 2800)
+        {
+            aliquota = 7.5;
+            restituicao = 142;
+        }
+
+        if (2800 < salario && salario <= 3751)
+        {
+            aliquota = 15;
+            restituicao = 350;
+        }
+
+        if (3750 < salario && salario <= 4664)
+        {
+            aliquota = 22.5;
+            restituicao = 636;
+        }
+
+        Console.WriteLine(
+            $"Sua alíquota do imposto de renda é de {aliquota}% e você pode restituir até R$ {restituicao}");
+    }
+}
