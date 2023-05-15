d4de151cc4c6931b74a0a5e2969d451426f1081f
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-15 10:57:13 -0300

    feat: add some exercices

---
+++ Fatorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        for (int counter = 1; counter <= 10; counter++)
+        {
+            int result = 1;
+            for (int fatorial = counter; fatorial > 0; fatorial--)
+            {
+                result = result * fatorial;
+            }
+            Console.WriteLine($"O fatorial de {counter} é {result}.");
+        }
+    }
+}
