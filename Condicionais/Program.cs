69e7675267fb2e0e4584e0ee5ec04ea5ccb94a60
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-15 07:17:45 -0300

    feat: add if-clause to demonstrate block scope

--- Condicionais/Program.cs
+++ Condicionais/Program.cs
@@ -7,17 +7,27 @@ class Programa
         Console.WriteLine("Programa 7 - Condicionais");
 
         int idadeJoao = 16;
-        int quantidadePessoas = 2;
+        int quantidadePessoas = 1;
 
         bool acompanhado = quantidadePessoas > 1;
-        bool grupo = true;
+        string textoAdicional;
+        
+        if (acompanhado)
+        {
+            textoAdicional = "João está acompanhado.";
+        } else
+        {
+            textoAdicional = "João não está acompanhado.";
+        }
 
         if (idadeJoao >= 18 || acompanhado)
         {
+            Console.WriteLine(textoAdicional);
             Console.WriteLine("Pode entrar!");
         }
         else
-        {
+        {   
+            Console.WriteLine(textoAdicional);
             Console.WriteLine("Não pode entrar.");
         }
 
